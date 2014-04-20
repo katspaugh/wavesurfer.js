e5b6f07a6e4bb86eb1ed18481b14806fd44498e3
katspaugh <katspaugh@yandex-team.ru>
2014-04-20 15:03:44 +0400

    add render-as-it-plays example

---
+++ example/render-as-it-plays/main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// Create an instance
+var wavesurfer = Object.create(WaveSurfer);
+
+// Init & load audio file
+document.addEventListener('DOMContentLoaded', function () {
+    // Init
+    wavesurfer.init({
+        container: document.querySelector('#waveform'),
+        waveColor: '#A8DBA8',
+        progressColor: '#3B8686',
+        minPxPerSec: 50,
+        maxDuration: 60
+    });
+
+    // Load audio from URL
+    wavesurfer.load('../panner/media.wav');
+
+    // Log errors
+    wavesurfer.on('error', function (msg) {
+        console.log(msg);
+    });
+
+    // Bind play/pause button
+    document.querySelector(
+        '[data-action="play"]'
+    ).addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
+
+    // Progress bar
+    (function () {
+        var progressDiv = document.querySelector('#progress-bar');
+        var progressBar = progressDiv.querySelector('.progress-bar');
+
+        var showProgress = function (percent) {
+            progressDiv.style.display = 'block';
+            progressBar.style.width = percent + '%';
+        };
+
+        var hideProgress = function () {
+            progressDiv.style.display = 'none';
+        };
+
+        wavesurfer.on('loading', showProgress);
+        wavesurfer.on('ready', hideProgress);
+        wavesurfer.on('destroy', hideProgress);
+        wavesurfer.on('error', hideProgress);
+    }());
+});
