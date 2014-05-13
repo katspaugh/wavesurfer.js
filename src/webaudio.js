7aaf7e2e946d79581cfd5169d642844eacd05bc3
pitrackster <patrick.guillou@u-grenoble3.fr>
2014-05-13 08:54:52 +0200

    finsih event

--- src/webaudio.js
+++ src/webaudio.js
@@ -1,51 +1,38 @@
 'use strict';
-
 WaveSurfer.WebAudio = {
     scriptBufferSize: 256,
     fftSize: 128,
-
-    getAudioContext: function () {
+    getAudioContext: function() {
         if (!(window.AudioContext || window.webkitAudioContext)) {
-            throw new Error(
-                'wavesurfer.js: your browser doesn\'t support WebAudio'
-            );
+            throw new Error('wavesurfer.js: your browser doesn\'t support WebAudio');
         }
-
         if (!WaveSurfer.WebAudio.audioContext) {
-            WaveSurfer.WebAudio.audioContext = new (
-                window.AudioContext || window.webkitAudioContext
-            );
+            WaveSurfer.WebAudio.audioContext = new(window.AudioContext || window.webkitAudioContext);
         }
         return WaveSurfer.WebAudio.audioContext;
     },
-
-    init: function (params) {
+    init: function(params) {
         this.params = params;
         this.ac = params.audioContext || this.getAudioContext();
-
         this.loop = false;
         this.prevFrameTime = 0;
         this.scheduledPause = null;
-
         this.createVolumeNode();
         this.createScriptNode();
         this.createAnalyserNode();
         this.setPlaybackRate(this.params.audioRate);
     },
-
-    loadBuffer: function (buffer) {
+    loadBuffer: function(buffer) {
         WaveSurfer.util.extend(this, WaveSurfer.WebAudio.Buffer);
         this.postInit();
         this.load(buffer);
     },
-
-    loadMedia: function (media) {
+    loadMedia: function(media) {
         WaveSurfer.util.extend(this, WaveSurfer.WebAudio.Media);
         this.postInit();
         this.load(media);
     },
-
-    disconnectFilters: function () {
+    disconnectFilters: function() {
         if (this.inputFilter) {
             this.inputFilter.disconnect();
         }
@@ -53,13 +40,10 @@ WaveSurfer.WebAudio = {
             this.outputFilter.disconnect();
         }
     },
-
-    setFilter: function (inputFilter, outputFilter) {
+    setFilter: function(inputFilter, outputFilter) {
         this.disconnectFilters();
-
         this.inputFilter = inputFilter;
         this.outputFilter = outputFilter || inputFilter;
-
         if (this.inputFilter && this.outputFilter) {
             this.analyser.connect(this.inputFilter);
             this.outputFilter.connect(this.gainNode);
@@ -67,8 +51,7 @@ WaveSurfer.WebAudio = {
             this.analyser.connect(this.gainNode);
         }
     },
-
-    createScriptNode: function () {
+    createScriptNode: function() {
         var my = this;
         var bufferSize = this.scriptBufferSize;
         if (this.ac.createScriptProcessor) {
@@ -77,50 +60,40 @@ WaveSurfer.WebAudio = {
             this.scriptNode = this.ac.createJavaScriptNode(bufferSize);
         }
         this.scriptNode.connect(this.ac.destination);
-        this.scriptNode.onaudioprocess = function () {
+        this.scriptNode.onaudioprocess = function() {
+            var time = my.getCurrentTime();
+            if (time > my.getDuration()) {
+                my.fireEvent('finish', time);
+            }
             if (!my.isPaused()) {
-                var time = my.getCurrentTime();
                 my.onPlayFrame(time);
                 my.fireEvent('audioprocess', time);
             }
         };
     },
-
-    onPlayFrame: function (time) {
+    onPlayFrame: function(time) {
         if (this.scheduledPause != null) {
             if (this.prevFrameTime >= this.scheduledPause) {
                 this.pause();
             }
         }
-
-        if (time > this.getDuration()) {
-            this.fireEvent('finish', time);
-        }
-
         if (this.loop) {
-            if (
-                this.prevFrameTime > this.loopStart &&
-                this.prevFrameTime <= this.loopEnd &&
-                time > this.loopEnd
-            ) {
+            if (this.prevFrameTime > this.loopStart && this.prevFrameTime <= this.loopEnd && time > this.loopEnd) {
                 this.play(this.loopStart);
             }
         }
-
         this.prevFrameTime = time;
     },
-
-    createAnalyserNode: function () {
+    createAnalyserNode: function() {
         this.analyser = this.ac.createAnalyser();
         this.analyser.fftSize = this.fftSize;
         this.analyserData = new Uint8Array(this.analyser.frequencyBinCount);
         this.analyser.connect(this.gainNode);
     },
-
     /**
      * Create the gain node needed to control the playback volume.
      */
-    createVolumeNode: function () {
+    createVolumeNode: function() {
         // Create gain node using the AudioContext
         if (this.ac.createGain) {
             this.gainNode = this.ac.createGain();
@@ -130,50 +103,45 @@ WaveSurfer.WebAudio = {
         // Add the gain node to the graph
         this.gainNode.connect(this.ac.destination);
     },
-
     /**
      * Set the gain to a new value.
      *
      * @param {Number} newGain The new gain, a floating point value
      * between 0 and 1. 0 being no gain and 1 being maximum gain.
      */
-    setVolume: function (newGain) {
+    setVolume: function(newGain) {
         this.gainNode.gain.value = newGain;
     },
-
     /**
      * Get the current gain.
      *
      * @returns {Number} The current gain, a floating point value
      * between 0 and 1. 0 being no gain and 1 being maximum gain.
      */
-    getVolume: function () {
+    getVolume: function() {
         return this.gainNode.gain.value;
     },
-
-    decodeArrayBuffer: function (arraybuffer, callback, errback) {
+    decodeArrayBuffer: function(arraybuffer, callback, errback) {
         var my = this;
-        this.ac.decodeAudioData(arraybuffer, function (data) {
+        this.ac.decodeAudioData(arraybuffer, function(data) {
             my.buffer = data;
             callback(data);
         }, errback);
     },
-
     /**
      * @returns {Float32Array} Array of peaks.
      */
-    getPeaks: function (length) {
+    getPeaks: function(length) {
         var buffer = this.buffer;
         var sampleSize = buffer.length / length;
-        var sampleStep = ~~(sampleSize / 10) || 1;
+        var sampleStep = ~~ (sampleSize / 10) || 1;
         var channels = buffer.numberOfChannels;
         var peaks = new Float32Array(length);
-
         for (var c = 0; c < channels; c++) {
             var chan = buffer.getChannelData(c);
             for (var i = 0; i < length; i++) {
-                var start = ~~(i * sampleSize);
-                var end = ~~(start + sampleSize);
+                var start = ~~ (i * sampleSize);
+                var end = ~~ (start + sampleSize);
                 var max = 0;
                 var min = 0;
                 for (var j = start; j < end; j += sampleStep) {
@@ -190,22 +158,18 @@ WaveSurfer.WebAudio = {
                 }
             }
         }
-
         return peaks;
     },
-
-    getPlayedPercents: function () {
+    getPlayedPercents: function() {
         var duration = this.getDuration();
         return (this.getCurrentTime() / duration) || 0;
     },
-
-    disconnectSource: function () {
+    disconnectSource: function() {
         if (this.source) {
             this.source.disconnect();
         }
     },
-
-    destroy: function () {
+    destroy: function() {
         this.pause();
         this.unAll();
         this.buffer = null;
@@ -215,65 +179,54 @@ WaveSurfer.WebAudio = {
         this.scriptNode.disconnect();
         this.analyser.disconnect();
     },
-
-    updateSelection: function (startPercent, endPercent) {
+    updateSelection: function(startPercent, endPercent) {
         var duration = this.getDuration();
         this.loop = true;
         this.loopStart = duration * startPercent;
         this.loopEnd = duration * endPercent;
     },
-
-    clearSelection: function () {
+    clearSelection: function() {
         this.loop = false;
         this.loopStart = 0;
         this.loopEnd = 0;
     },
-
     /**
      * Returns the real-time waveform data.
      *
      * @return {Uint8Array} The frequency data.
      * Values range from 0 to 255.
      */
-    waveform: function () {
+    waveform: function() {
         this.analyser.getByteTimeDomainData(this.analyserData);
         return this.analyserData;
     },
-
-
     /* Dummy methods */
-
-    postInit: function () {},
-    load: function () {},
-
+    postInit: function() {},
+    load: function() {},
     /**
      * Get current position in seconds.
      */
-    getCurrentTime: function () {
+    getCurrentTime: function() {
         return 0;
     },
-
     /**
      * @returns {Boolean}
      */
-    isPaused: function () {
+    isPaused: function() {
         return true;
     },
-
     /**
      * Get duration in seconds.
      */
-    getDuration: function () {
+    getDuration: function() {
         return 0;
     },
-
     /**
      * Set the audio source playback rate.
      */
-    setPlaybackRate: function (value) {
+    setPlaybackRate: function(value) {
         this.playbackRate = value || 1;
     },
-
     /**
      * Plays the loaded audio region.
      *
@@ -282,12 +235,10 @@ WaveSurfer.WebAudio = {
      * @param {Number} end When to stop
      * relative to the beginning of a clip.
      */
-    play: function (start, end) {},
-
+    play: function(start, end) {},
     /**
      * Pauses the loaded audio.
      */
-    pause: function () {}
+    pause: function() {}
 };
-
 WaveSurfer.util.extend(WaveSurfer.WebAudio, WaveSurfer.Observer);
