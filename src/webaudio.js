37acc62e1489a2be35109fa5b3f05dbd98764344
Kage1989 <kwok.hoe.89@gmail.com>
2015-02-16 23:35:00 +0800

    Update webaudio.js

--- src/webaudio.js
+++ src/webaudio.js
@@ -196,6 +196,45 @@ WaveSurfer.WebAudio = {
 
         return peaks;
     },
+    
+	/**
+     * @returns {Float32Array} Array of peaks for both channel.
+     */
+    getPeaksMainWaveform: function (length) {
+        var buffer = this.buffer;
+        var sampleSize = buffer.length / length;
+        var sampleStep = ~~(sampleSize / 10) || 1;
+        var channels = buffer.numberOfChannels;
+        var peaks = new Float32Array(channels*length);
+
+        for (var c = 0; c < channels; c++) {
+            var chan = buffer.getChannelData(c);
+            for (var i = 0; i < length; i++) {
+                var start = ~~(i * sampleSize);
+                var end = ~~(start + sampleSize);
+                var max = 0;
+                for (var j = start; j < end; j += sampleStep) {
+                    var value = chan[j];
+                    if (value > max) {
+                        max = value;
+                    // faster than Math.abs
+                    } else if (-value > max) {
+                        max = -value;
+                    }
+                }
+                
+				//Sequentially
+				peaks[c*length+i] = max;
+
+            }
+        }
+
+        return peaks;
+    },
+    
+    getNumberOfChannels: function () {
+		return this.buffer.numberOfChannels || 1;
+    },
 
     getPlayedPercents: function () {
         return this.state.getPlayedPercents.call(this);
