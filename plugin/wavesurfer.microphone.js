e52c15b7ff85b08ff176d3c31f0234af2e2f763e
skorlir <skorlir@gmail.com>
2016-02-22 10:20:49 -0600

    Remove pre-existing headers that are no longer needed

--- plugin/wavesurfer.microphone.js
+++ plugin/wavesurfer.microphone.js
@@ -1,218 +1,210 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['wavesurfer'], factory);
-    } else {
-        root.WaveSurfer.Microphone = factory(root.WaveSurfer);
+'use strict';
+
+WaveSurfer.Microphone = {
+init: function (params) {
+    this.params = params;
+
+    var wavesurfer = this.wavesurfer = params.wavesurfer;
+
+    if (!this.wavesurfer) {
+	throw new Error('No WaveSurfer instance provided');
     }
-}(this, function (WaveSurfer) {
-    'use strict';
 
-    WaveSurfer.Microphone = {
-        init: function (params) {
-            this.params = params;
+    this.active = false;
+    this.paused = false;
+    this.reloadBufferFunction = this.reloadBuffer.bind(this);
 
-            var wavesurfer = this.wavesurfer = params.wavesurfer;
+    // cross-browser getUserMedia
+    var getUserMediaFn =
+	navigator.getUserMedia ||
+	navigator.webkitGetUserMedia ||
+	navigator.mozGetUserMedia ||
+	navigator.msGetUserMedia;
 
-            if (!this.wavesurfer) {
-                throw new Error('No WaveSurfer instance provided');
-            }
+    if (getUserMediaFn) {
+	this.getUserMedia = getUserMediaFn.bind(navigator);
+    } else {
+	this.getUserMedia = function (constraints, successCallback, errorCallback) {
+	    errorCallback(new Error('getUserMedia is not supported'));
+	};
+    }
 
-            this.active = false;
-            this.paused = false;
-            this.reloadBufferFunction = this.reloadBuffer.bind(this);
+    // The buffer size in units of sample-frames.
+    // If specified, the bufferSize must be one of the following values:
+    // 256, 512, 1024, 2048, 4096, 8192, 16384. Defaults to 4096.
+    this.bufferSize = this.params.bufferSize || 4096;
 
-            // cross-browser getUserMedia
-            var getUserMediaFn =
-                navigator.getUserMedia ||
-                navigator.webkitGetUserMedia ||
-                navigator.mozGetUserMedia ||
-                navigator.msGetUserMedia;
+    // Integer specifying the number of channels for this node's input,
+    // defaults to 1. Values of up to 32 are supported.
+    this.numberOfInputChannels = this.params.numberOfInputChannels || 1;
 
-            if (getUserMediaFn) {
-                this.getUserMedia = getUserMediaFn.bind(navigator);
-            } else {
-                this.getUserMedia = function (constraints, successCallback, errorCallback) {
-                    errorCallback(new Error('getUserMedia is not supported'));
-                };
-            }
+    // Integer specifying the number of channels for this node's output,
+    // defaults to 1. Values of up to 32 are supported.
+    this.numberOfOutputChannels = this.params.numberOfOutputChannels || 1;
 
-            // The buffer size in units of sample-frames.
-            // If specified, the bufferSize must be one of the following values:
-            // 256, 512, 1024, 2048, 4096, 8192, 16384. Defaults to 4096.
-            this.bufferSize = this.params.bufferSize || 4096;
+    // wavesurfer's AudioContext where we'll route the mic signal to
+    this.micContext = this.wavesurfer.backend.getAudioContext();
+},
 
-            // Integer specifying the number of channels for this node's input,
-            // defaults to 1. Values of up to 32 are supported.
-            this.numberOfInputChannels = this.params.numberOfInputChannels || 1;
+/**
+ * Allow user to select audio input device, eg. microphone, and
+ * start the visualization.
+ */
+start: function() {
+    this.getUserMedia({
+	video: false,
+	audio: true
+    },
+    this.gotStream.bind(this),
+    this.deviceError.bind(this));
+},
 
-            // Integer specifying the number of channels for this node's output,
-            // defaults to 1. Values of up to 32 are supported.
-            this.numberOfOutputChannels = this.params.numberOfOutputChannels || 1;
+/**
+ * Pause/resume visualization.
+ */
+togglePlay: function() {
+    if (!this.active) {
+	// start it first
+	this.start();
+    } else {
+	// toggle paused
+	this.paused = !this.paused;
 
-            // wavesurfer's AudioContext where we'll route the mic signal to
-            this.micContext = this.wavesurfer.backend.getAudioContext();
-        },
+	if (this.paused) {
+	    this.pause();
+	} else {
+	    this.play();
+	}
+    }
+},
 
-        /**
-         * Allow user to select audio input device, eg. microphone, and
-         * start the visualization.
-         */
-        start: function() {
-            this.getUserMedia({
-                video: false,
-                audio: true
-            },
-            this.gotStream.bind(this),
-            this.deviceError.bind(this));
-        },
+/**
+ * Play visualization.
+ */
+play: function() {
+    this.paused = false;
 
-        /**
-         * Pause/resume visualization.
-         */
-        togglePlay: function() {
-            if (!this.active) {
-                // start it first
-                this.start();
-            } else {
-                // toggle paused
-                this.paused = !this.paused;
+    this.connect();
+},
 
-                if (this.paused) {
-                    this.pause();
-                } else {
-                    this.play();
-                }
-            }
-        },
+/**
+ * Pause visualization.
+ */
+pause: function() {
+    this.paused = true;
 
-        /**
-         * Play visualization.
-         */
-        play: function() {
-            this.paused = false;
+    // disconnect sources so they can be used elsewhere
+    // (eg. during audio playback)
+    this.disconnect();
+},
 
-            this.connect();
-        },
+/**
+ * Stop the device stream and remove any remaining waveform drawing from
+ * the wavesurfer canvas.
+ */
+stop: function() {
+    if (this.active) {
+	// stop visualization and device
+	this.stopDevice();
 
-        /**
-         * Pause visualization.
-         */
-        pause: function() {
-            this.paused = true;
+	// empty last frame
+	this.wavesurfer.empty();
+    }
+},
 
-            // disconnect sources so they can be used elsewhere
-            // (eg. during audio playback)
-            this.disconnect();
-        },
+/**
+ * Stop the device and the visualization.
+ */
+stopDevice: function() {
+    this.active = false;
 
-        /**
-         * Stop the device stream and remove any remaining waveform drawing from
-         * the wavesurfer canvas.
-         */
-        stop: function() {
-            if (this.active) {
-                // stop visualization and device
-                this.stopDevice();
+    // stop visualization
+    this.disconnect();
 
-                // empty last frame
-                this.wavesurfer.empty();
-            }
-        },
+    // stop stream from device
+    if (this.stream) {
+	this.stream.stop();
+    }
+},
 
-        /**
-         * Stop the device and the visualization.
-         */
-        stopDevice: function() {
-            this.active = false;
+/**
+ * Connect the media sources that feed the visualization.
+ */
+connect: function() {
+    if (this.stream !== undefined) {
+	// Create an AudioNode from the stream.
+	this.mediaStreamSource = this.micContext.createMediaStreamSource(this.stream);
 
-            // stop visualization
-            this.disconnect();
+	this.levelChecker = this.micContext.createScriptProcessor(
+	    this.bufferSize, this.numberOfInputChannels, this.numberOfOutputChannels);
+	this.mediaStreamSource.connect(this.levelChecker);
 
-            // stop stream from device
-            if (this.stream) {
-                this.stream.stop();
-            }
-        },
+	this.levelChecker.connect(this.micContext.destination);
+	this.levelChecker.onaudioprocess = this.reloadBufferFunction;
+    }
+},
 
-        /**
-         * Connect the media sources that feed the visualization.
-         */
-        connect: function() {
-            if (this.stream !== undefined) {
-                // Create an AudioNode from the stream.
-                this.mediaStreamSource = this.micContext.createMediaStreamSource(this.stream);
+/**
+ * Disconnect the media sources that feed the visualization.
+ */
+disconnect: function() {
+    if (this.mediaStreamSource !== undefined) {
+	this.mediaStreamSource.disconnect();
+    }
 
-                this.levelChecker = this.micContext.createScriptProcessor(
-                    this.bufferSize, this.numberOfInputChannels, this.numberOfOutputChannels);
-                this.mediaStreamSource.connect(this.levelChecker);
+    if (this.levelChecker !== undefined) {
+	this.levelChecker.disconnect();
+	this.levelChecker.onaudioprocess = undefined;
+    }
+},
 
-                this.levelChecker.connect(this.micContext.destination);
-                this.levelChecker.onaudioprocess = this.reloadBufferFunction;
-            }
-        },
+/**
+ * Redraw the waveform.
+ */
+reloadBuffer: function(event) {
+    if (!this.paused) {
+	this.wavesurfer.empty();
+	this.wavesurfer.loadDecodedBuffer(event.inputBuffer);
+    }
+},
 
-        /**
-         * Disconnect the media sources that feed the visualization.
-         */
-        disconnect: function() {
-            if (this.mediaStreamSource !== undefined) {
-                this.mediaStreamSource.disconnect();
-            }
+/**
+ * Audio input device is ready.
+ *
+ * @param {LocalMediaStream} stream: the microphone's media stream.
+ */
+gotStream: function(stream) {
+    this.stream = stream;
+    this.active = true;
 
-            if (this.levelChecker !== undefined) {
-                this.levelChecker.disconnect();
-                this.levelChecker.onaudioprocess = undefined;
-            }
-        },
+    // start visualization
+    this.play();
 
-        /**
-         * Redraw the waveform.
-         */
-        reloadBuffer: function(event) {
-            if (!this.paused) {
-                this.wavesurfer.empty();
-                this.wavesurfer.loadDecodedBuffer(event.inputBuffer);
-            }
-        },
+    // notify listeners
+    this.fireEvent('deviceReady', stream);
+},
 
-        /**
-         * Audio input device is ready.
-         *
-         * @param {LocalMediaStream} stream: the microphone's media stream.
-         */
-        gotStream: function(stream) {
-            this.stream = stream;
-            this.active = true;
+/**
+ * Destroy the microphone plugin.
+ */
+destroy: function(event) {
+    // make sure the buffer is not redrawn during
+    // cleanup and demolition of this plugin.
+    this.paused = true;
 
-            // start visualization
-            this.play();
+    this.stop();
+},
 
-            // notify listeners
-            this.fireEvent('deviceReady', stream);
-        },
+/**
+ * Device error callback.
+ */
+deviceError: function(code) {
+    // notify listeners
+    this.fireEvent('deviceError', code);
+}
 
-        /**
-         * Destroy the microphone plugin.
-         */
-        destroy: function(event) {
-            // make sure the buffer is not redrawn during
-            // cleanup and demolition of this plugin.
-            this.paused = true;
+};
 
-            this.stop();
-        },
+WaveSurfer.util.extend(WaveSurfer.Microphone, WaveSurfer.Observer);
 
-        /**
-         * Device error callback.
-         */
-        deviceError: function(code) {
-            // notify listeners
-            this.fireEvent('deviceError', code);
-        }
-
-    };
-
-    WaveSurfer.util.extend(WaveSurfer.Microphone, WaveSurfer.Observer);
-
-    return WaveSurfer.Microphone;
-}));
+return WaveSurfer.Microphone;
--- plugin/wavesurfer.timeline.js
+++ plugin/wavesurfer.timeline.js
@@ -1,209 +1,201 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['wavesurfer'], factory);
-    } else {
-        root.WaveSurfer.Timeline = factory(root.WaveSurfer);
+'use strict';
+
+WaveSurfer.Timeline = {
+init: function (params) {
+    this.params = params;
+    var wavesurfer = this.wavesurfer = params.wavesurfer;
+
+    if (!this.wavesurfer) {
+	throw Error('No WaveSurfer intance provided');
     }
-}(this, function (WaveSurfer) {
-    'use strict';
 
-    WaveSurfer.Timeline = {
-        init: function (params) {
-            this.params = params;
-            var wavesurfer = this.wavesurfer = params.wavesurfer;
+    var drawer = this.drawer = this.wavesurfer.drawer;
 
-            if (!this.wavesurfer) {
-                throw Error('No WaveSurfer intance provided');
-            }
+    this.container = 'string' == typeof params.container ?
+	document.querySelector(params.container) : params.container;
 
-            var drawer = this.drawer = this.wavesurfer.drawer;
+    if (!this.container) {
+	throw Error('No container for WaveSurfer timeline');
+    }
 
-            this.container = 'string' == typeof params.container ?
-                document.querySelector(params.container) : params.container;
+    this.width = drawer.width;
+    this.height = this.params.height || 20;
+    this.notchPercentHeight = this.params.notchPercentHeight || 90;
+    this.primaryColor = this.params.primaryColor || '#000';
+    this.secondaryColor = this.params.secondaryColor || '#c0c0c0';
+    this.primaryFontColor = this.params.primaryFontColor || '#000';
+    this.secondaryFontColor = this.params.secondaryFontColor || '#000';
+    this.fontFamily = this.params.fontFamily || 'Arial';
+    this.fontSize = this.params.fontSize || 10;
+    this.timeInterval = this.params.timeInterval;
+    this.primaryLabelInterval = this.params.primaryLabelInterval;
+    this.secondaryLabelInterval = this.params.secondaryLabelInterval;
+    this.formatTimeCallback = this.params.formatTimeCallback;
 
-            if (!this.container) {
-                throw Error('No container for WaveSurfer timeline');
-            }
+    this.createWrapper();
+    this.createCanvas();
+    this.render();
 
-            this.width = drawer.width;
-            this.height = this.params.height || 20;
-            this.notchPercentHeight = this.params.notchPercentHeight || 90;
-            this.primaryColor = this.params.primaryColor || '#000';
-            this.secondaryColor = this.params.secondaryColor || '#c0c0c0';
-            this.primaryFontColor = this.params.primaryFontColor || '#000';
-            this.secondaryFontColor = this.params.secondaryFontColor || '#000';
-            this.fontFamily = this.params.fontFamily || 'Arial';
-            this.fontSize = this.params.fontSize || 10;
-            this.timeInterval = this.params.timeInterval;
-            this.primaryLabelInterval = this.params.primaryLabelInterval;
-            this.secondaryLabelInterval = this.params.secondaryLabelInterval;
-            this.formatTimeCallback = this.params.formatTimeCallback;
+    wavesurfer.drawer.wrapper.onscroll = this.updateScroll.bind(this);
+    wavesurfer.on('redraw', this.render.bind(this));
+    wavesurfer.on('destroy', this.destroy.bind(this));
+},
 
-            this.createWrapper();
-            this.createCanvas();
-            this.render();
+destroy: function () {
+    this.unAll();
+    if (this.wrapper && this.wrapper.parentNode) {
+	this.wrapper.parentNode.removeChild(this.wrapper);
+	this.wrapper = null;
+    }
+},
 
-            wavesurfer.drawer.wrapper.onscroll = this.updateScroll.bind(this);
-            wavesurfer.on('redraw', this.render.bind(this));
-            wavesurfer.on('destroy', this.destroy.bind(this));
-        },
+createWrapper: function () {
+    var prevTimeline = this.container.querySelector('timeline');
+    if (prevTimeline) {
+	this.container.removeChild(prevTimeline);
+    }
 
-        destroy: function () {
-            this.unAll();
-            if (this.wrapper && this.wrapper.parentNode) {
-                this.wrapper.parentNode.removeChild(this.wrapper);
-                this.wrapper = null;
-            }
-        },
+    var wsParams = this.wavesurfer.params;
+    this.wrapper = this.container.appendChild(
+	document.createElement('timeline')
+    );
+    this.drawer.style(this.wrapper, {
+	display: 'block',
+	position: 'relative',
+	userSelect: 'none',
+	webkitUserSelect: 'none',
+	height: this.height + 'px'
+    });
 
-        createWrapper: function () {
-            var prevTimeline = this.container.querySelector('timeline');
-            if (prevTimeline) {
-                this.container.removeChild(prevTimeline);
-            }
+    if (wsParams.fillParent || wsParams.scrollParent) {
+	this.drawer.style(this.wrapper, {
+	    width: '100%',
+	    overflowX: 'hidden',
+	    overflowY: 'hidden'
+	});
+    }
 
-            var wsParams = this.wavesurfer.params;
-            this.wrapper = this.container.appendChild(
-                document.createElement('timeline')
-            );
-            this.drawer.style(this.wrapper, {
-                display: 'block',
-                position: 'relative',
-                userSelect: 'none',
-                webkitUserSelect: 'none',
-                height: this.height + 'px'
-            });
+    var my = this;
+    this.wrapper.addEventListener('click', function (e) {
+	e.preventDefault();
+	var relX = 'offsetX' in e ? e.offsetX : e.layerX;
+	my.fireEvent('click', (relX / my.wrapper.scrollWidth) || 0);
+    });
+},
 
-            if (wsParams.fillParent || wsParams.scrollParent) {
-                this.drawer.style(this.wrapper, {
-                    width: '100%',
-                    overflowX: 'hidden',
-                    overflowY: 'hidden'
-                });
-            }
+createCanvas: function () {
+    var canvas = this.canvas = this.wrapper.appendChild(
+	document.createElement('canvas')
+    );
 
-            var my = this;
-            this.wrapper.addEventListener('click', function (e) {
-                e.preventDefault();
-                var relX = 'offsetX' in e ? e.offsetX : e.layerX;
-                my.fireEvent('click', (relX / my.wrapper.scrollWidth) || 0);
-            });
-        },
+    this.timeCc = canvas.getContext('2d');
 
-        createCanvas: function () {
-            var canvas = this.canvas = this.wrapper.appendChild(
-                document.createElement('canvas')
-            );
+    this.wavesurfer.drawer.style(canvas, {
+	position: 'absolute',
+	zIndex: 4
+    });
+},
 
-            this.timeCc = canvas.getContext('2d');
+render: function () {
+    this.updateCanvasStyle();
+    this.drawTimeCanvas();
+},
 
-            this.wavesurfer.drawer.style(canvas, {
-                position: 'absolute',
-                zIndex: 4
-            });
-        },
+updateCanvasStyle: function () {
+    var width = this.drawer.wrapper.scrollWidth;
+    this.canvas.width = width * this.wavesurfer.params.pixelRatio;
+    this.canvas.height = this.height * this.wavesurfer.params.pixelRatio;
+    this.canvas.style.width = width + 'px';
+    this.canvas.style.height = this.height + 'px';
+},
 
-        render: function () {
-            this.updateCanvasStyle();
-            this.drawTimeCanvas();
-        },
+drawTimeCanvas: function() {
+    var backend = this.wavesurfer.backend,
+	wsParams = this.wavesurfer.params,
+	duration = backend.getDuration(),
+	self = this;
 
-        updateCanvasStyle: function () {
-            var width = this.drawer.wrapper.scrollWidth;
-            this.canvas.width = width * this.wavesurfer.params.pixelRatio;
-            this.canvas.height = this.height * this.wavesurfer.params.pixelRatio;
-            this.canvas.style.width = width + 'px';
-            this.canvas.style.height = this.height + 'px';
-        },
+    if (wsParams.fillParent && !wsParams.scrollParent) {
+	var width = this.drawer.getWidth();
+    } else {
+	width = this.drawer.wrapper.scrollWidth * wsParams.pixelRatio;
+    }
+    var pixelsPerSecond = width/duration;
 
-        drawTimeCanvas: function() {
-            var backend = this.wavesurfer.backend,
-                wsParams = this.wavesurfer.params,
-                duration = backend.getDuration(),
-                self = this;
+    if (duration > 0) {
+	var curPixel = 0,
+	    curSeconds = 0,
+	    totalSeconds = parseInt(duration, 10) + 1,
+	    formatTime = function(seconds) {
+		if (typeof self.formatTimeCallback === 'function') {
+		    return self.formatTimeCallback(seconds);
+		}
 
-            if (wsParams.fillParent && !wsParams.scrollParent) {
-                var width = this.drawer.getWidth();
-            } else {
-                width = this.drawer.wrapper.scrollWidth * wsParams.pixelRatio;
-            }
-            var pixelsPerSecond = width/duration;
+		if (seconds/60 > 1) {
+		    var minutes = parseInt(seconds / 60),
+			seconds = parseInt(seconds % 60);
+		    seconds = (seconds < 10) ? '0' + seconds : seconds;
+		    return '' + minutes + ':' + seconds;
+		} else {
+		    return seconds;
+		}
+	    };
 
-            if (duration > 0) {
-                var curPixel = 0,
-                    curSeconds = 0,
-                    totalSeconds = parseInt(duration, 10) + 1,
-                    formatTime = function(seconds) {
-                        if (typeof self.formatTimeCallback === 'function') {
-                            return self.formatTimeCallback(seconds);
-                        }
+	if (pixelsPerSecond * 1 >= 25) {
+	    var timeInterval = 1;
+	    var primaryLabelInterval = 10;
+	    var secondaryLabelInterval = 5;
+	} else if (pixelsPerSecond * 5 >= 25) {
+	    var timeInterval = 5;
+	    var primaryLabelInterval = 6;
+	    var secondaryLabelInterval = 2;
+	} else if (pixelsPerSecond * 15 >= 25) {
+	    var timeInterval = 15;
+	    var primaryLabelInterval = 4;
+	    var secondaryLabelInterval = 2;
+	} else {
+	    var timeInterval = 60;
+	    var primaryLabelInterval = 4;
+	    var secondaryLabelInterval = 2;
+	}
 
-                        if (seconds/60 > 1) {
-                            var minutes = parseInt(seconds / 60),
-                                seconds = parseInt(seconds % 60);
-                            seconds = (seconds < 10) ? '0' + seconds : seconds;
-                            return '' + minutes + ':' + seconds;
-                        } else {
-                            return seconds;
-                        }
-                    };
+	timeInterval = this.timeInterval || timeInterval;
+	primaryLabelInterval = this.primaryLabelInterval || primaryLabelInterval;
+	secondaryLabelInterval = this.secondaryLabelInterval || secondaryLabelInterval;
 
-                if (pixelsPerSecond * 1 >= 25) {
-                    var timeInterval = 1;
-                    var primaryLabelInterval = 10;
-                    var secondaryLabelInterval = 5;
-                } else if (pixelsPerSecond * 5 >= 25) {
-                    var timeInterval = 5;
-                    var primaryLabelInterval = 6;
-                    var secondaryLabelInterval = 2;
-                } else if (pixelsPerSecond * 15 >= 25) {
-                    var timeInterval = 15;
-                    var primaryLabelInterval = 4;
-                    var secondaryLabelInterval = 2;
-                } else {
-                    var timeInterval = 60;
-                    var primaryLabelInterval = 4;
-                    var secondaryLabelInterval = 2;
-                }
+	var height1 = this.height - 4,
+	    height2 = (this.height * (this.notchPercentHeight / 100.0)) - 4,
+	    fontSize = this.fontSize * wsParams.pixelRatio;
 
-                timeInterval = this.timeInterval || timeInterval;
-                primaryLabelInterval = this.primaryLabelInterval || primaryLabelInterval;
-                secondaryLabelInterval = this.secondaryLabelInterval || secondaryLabelInterval;
+	for (var i = 0; i < totalSeconds/timeInterval; i++) {
+	    if (i % primaryLabelInterval == 0) {
+		this.timeCc.fillStyle = this.primaryColor;
+		this.timeCc.fillRect(curPixel, 0, 1, height1);
+		this.timeCc.font = fontSize + 'px ' + this.fontFamily;
+		this.timeCc.fillStyle = this.primaryFontColor;
+		this.timeCc.fillText(formatTime(curSeconds), curPixel + 5, height1);
+	    } else if (i % secondaryLabelInterval == 0) {
+		this.timeCc.fillStyle = this.secondaryColor;
+		this.timeCc.fillRect(curPixel, 0, 1, height1);
+		this.timeCc.font = fontSize + 'px ' + this.fontFamily;
+		this.timeCc.fillStyle = this.secondaryFontColor;
+		this.timeCc.fillText(formatTime(curSeconds), curPixel + 5, height1);
+	    } else {
+		this.timeCc.fillStyle = this.secondaryColor;
+		this.timeCc.fillRect(curPixel, 0, 1, height2);
+	    }
 
-                var height1 = this.height - 4,
-                    height2 = (this.height * (this.notchPercentHeight / 100.0)) - 4,
-                    fontSize = this.fontSize * wsParams.pixelRatio;
+	    curSeconds += timeInterval;
+	    curPixel += pixelsPerSecond * timeInterval;
+	}
+    }
+},
 
-                for (var i = 0; i < totalSeconds/timeInterval; i++) {
-                    if (i % primaryLabelInterval == 0) {
-                        this.timeCc.fillStyle = this.primaryColor;
-                        this.timeCc.fillRect(curPixel, 0, 1, height1);
-                        this.timeCc.font = fontSize + 'px ' + this.fontFamily;
-                        this.timeCc.fillStyle = this.primaryFontColor;
-                        this.timeCc.fillText(formatTime(curSeconds), curPixel + 5, height1);
-                    } else if (i % secondaryLabelInterval == 0) {
-                        this.timeCc.fillStyle = this.secondaryColor;
-                        this.timeCc.fillRect(curPixel, 0, 1, height1);
-                        this.timeCc.font = fontSize + 'px ' + this.fontFamily;
-                        this.timeCc.fillStyle = this.secondaryFontColor;
-                        this.timeCc.fillText(formatTime(curSeconds), curPixel + 5, height1);
-                    } else {
-                        this.timeCc.fillStyle = this.secondaryColor;
-                        this.timeCc.fillRect(curPixel, 0, 1, height2);
-                    }
+updateScroll: function () {
+    this.wrapper.scrollLeft = this.drawer.wrapper.scrollLeft;
+}
+};
 
-                    curSeconds += timeInterval;
-                    curPixel += pixelsPerSecond * timeInterval;
-                }
-            }
-        },
+WaveSurfer.util.extend(WaveSurfer.Timeline, WaveSurfer.Observer);
 
-        updateScroll: function () {
-            this.wrapper.scrollLeft = this.drawer.wrapper.scrollLeft;
-        }
-    };
-
-    WaveSurfer.util.extend(WaveSurfer.Timeline, WaveSurfer.Observer);
-
-    return WaveSurfer.Timeline;
-}));
+return WaveSurfer.Timeline;
