aaf818b2e07f9ed57ecdddfc001ad3a00ae0d916
Filip Zrůst <frzng@me.com>
2016-05-11 23:04:38 +0200

    MediaElement#getDuration throwing TypeError fixed
    WaveSurfer.MediaElement#getDuration throwed `TypeError: Not enough
    arguments` in Safari 9.1. It also violated contract specified in [HTML
    Time
    ranges](https://developers.whatwg.org/the-video-element.html#dom-timeranges-end).
    
    This should fix #719.

--- src/mediaelement.js
+++ src/mediaelement.js
@@ -67,7 +67,7 @@ WaveSurfer.util.extend(WaveSurfer.MediaElement, {
     getDuration: function () {
         var duration = this.media.duration;
         if (duration >= Infinity) { // streaming audio
-            duration = this.media.seekable.end();
+            duration = this.media.seekable.end(0);
         }
         return duration;
     },
