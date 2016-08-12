5b8157821bf94d449177931abac87863633aab08
Chris Parton <chrisparton1991@gmail.com>
2016-08-12 18:13:45 +1000

    778 - Multicanvas Timeline Zoom Scrolling Issue (#792)
    * Fix offset and width calculations for last canvas in timeline.
    
    * 778 - Multicanvas Timeline Zoom Scrolling Issue

--- plugin/wavesurfer.timeline.js
+++ plugin/wavesurfer.timeline.js
@@ -37,7 +37,6 @@ WaveSurfer.Timeline = {
         this.canvases = [];
 
         this.createWrapper();
-        this.createCanvases();
         this.render();
 
         drawer.wrapper.addEventListener('scroll', function (e) {
@@ -89,7 +88,16 @@ WaveSurfer.Timeline = {
         });
     },
 
+    removeOldCanvases: function () {
+        while (this.canvases.length > 0) {
+            var canvas = this.canvases.pop();
+            canvas.parentElement.removeChild(canvas);
+        }
+    },
+
     createCanvases: function () {
+        this.removeOldCanvases();
+
         var totalWidth = Math.round(this.drawer.wrapper.scrollWidth),
             requiredCanvases = Math.ceil(totalWidth / this.maxCanvasElementWidth),
             canvas;
@@ -105,6 +113,7 @@ WaveSurfer.Timeline = {
     },
 
     render: function () {
+        this.createCanvases();
         this.updateCanvasStyle();
         this.drawTimeCanvases();
     },
@@ -116,14 +125,14 @@ WaveSurfer.Timeline = {
                 canvasWidth = this.maxCanvasElementWidth;
 
             if (i === requiredCanvases - 1) {
-                canvasWidth = this.width / this.pixelRatio - (this.maxCanvasElementWidth * (requiredCanvases - 1));
+                canvasWidth = this.drawer.wrapper.scrollWidth - (this.maxCanvasElementWidth * (requiredCanvases - 1));
             }
 
             canvas.width = canvasWidth * this.pixelRatio;
             canvas.height = this.height * this.pixelRatio;
             canvas.style.width = canvasWidth + 'px';
             canvas.style.height = this.height + 'px';
-            canvas.style.left = i * canvasWidth + 'px';
+            canvas.style.left = i * this.maxCanvasElementWidth + 'px';
         }
     },
 
