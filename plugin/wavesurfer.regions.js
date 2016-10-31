c866de225087e3802b17d7dd78822d67b9d81cda
Sam Hulick <ffxsam@users.noreply.github.com>
2016-10-30 23:39:34 -0700

    Fixed a bug in Firefox and scrollbar usage

--- plugin/wavesurfer.regions.js
+++ plugin/wavesurfer.regions.js
@@ -42,6 +42,12 @@ WaveSurfer.Regions = {
 
         var eventDown = function (e) {
             if (e.touches && e.touches.length > 1) { return; }
+
+            // Check whether the click/tap is on the bottom-most DOM element
+            // Effectively prevent clicks on the scrollbar from registering as
+            // region creation.
+            if (e.target.childElementCount > 0) { return; }
+
             touchId = e.targetTouches ? e.targetTouches[0].identifier : null;
 
             drag = true;
