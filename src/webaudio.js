6d5c2c803966264e47e7697834c0d90913ab6362
entonbiba <entonbiba@users.noreply.github.com>
2017-01-27 11:14:09 -0500

    Update webaudio.js

--- src/webaudio.js
+++ src/webaudio.js
@@ -402,8 +402,7 @@ WaveSurfer.WebAudio = {
     getCurrentTime: function () {
         return this.state.getCurrentTime.call(this);
     },
-    
-    
+        
     /**
     *   Returns the current playback rate.
     */
