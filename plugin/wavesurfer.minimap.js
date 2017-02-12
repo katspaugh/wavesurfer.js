1f0887eead9740b8612e66c0f759de7b6a684e9f
entonbiba <entonbiba@users.noreply.github.com>
2017-02-11 22:15:42 -0500

    add debounce to resize/orientationchange event for minimap plugin (#987)
    * add debounce to resize event
    * add orientationchange event

--- plugin/wavesurfer.minimap.js
+++ plugin/wavesurfer.minimap.js
@@ -118,14 +118,15 @@ WaveSurfer.Minimap = WaveSurfer.util.extend({}, WaveSurfer.Drawer, WaveSurfer.Dr
         }
 
         var prevWidth = 0;
-        var onResize = function () {
+        var onResize = this.wavesurfer.util.debounce(function () {
             if (prevWidth != my.wrapper.clientWidth) {
                 prevWidth = my.wrapper.clientWidth;
                 my.render();
                 my.progress(my.wavesurfer.backend.getPlayedPercents());
             }
-        };
+        }, 100);
         window.addEventListener('resize', onResize, true);
+        window.addEventListener('orientationchange', onResize, true);
 
         this.wavesurfer.on('destroy', function () {
             my.destroy.bind(this);
