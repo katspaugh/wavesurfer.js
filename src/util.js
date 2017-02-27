8eef4f20581e6da5b5ee9f63ae1a48c80f335acd
entonbiba <entonbiba@users.noreply.github.com>
2017-02-27 12:57:21 -0500

    create playlist parser for m3u, pls, smil and json playlists
    create playlist parser for m3u, pls, smil and json playlists

--- src/util.js
+++ src/util.js
@@ -94,6 +94,33 @@ WaveSurfer.util = {
         xhr.send();
         ajax.xhr = xhr;
         return ajax;
+    },
+
+    parsePlaylist: function (plfile,pltype) {
+        // check if playlist type is given
+        if (pltype == 'm3u' || 'audio/mpegurl') {
+            var playlist = plfile.replace(/^.*#.*$|#EXTM3U|#EXTINF:/mg, '').split('\n');
+        } else if (pltype == 'pls' || 'audio/x-scpls') {
+            // to do
+        } else if (pltype == 'smil' || pltype == 'application/smil'){
+            // to do
+        } else if (pltype == 'json' || pltype == 'application/json'){
+		    // to do
+	    } else {
+            return [];
+        }
+
+        // playlist type is set return the playlist
+        var outputArray = [];
+        for (var i = 0; i < playlist.length; i++){
+            if (playlist[i]) {
+                // check if file name has .mp3 or .wav before adding the playlist array
+                if(playlist[i].indexOf('mp3') !== -1 || playlist[i].indexOf('wav') !== -1){
+                    outputArray.push(playlist[i]);
+                }
+            }
+        }
+        return outputArray;
     }
 };
 
