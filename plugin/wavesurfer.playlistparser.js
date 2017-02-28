62ff8e8d873292c897fe602efb663e358c3e24c9
entonbiba <entonbiba@users.noreply.github.com>
2017-02-28 08:31:00 -0500

    create playlist parser plugin

---
+++ plugin/wavesurfer.playlistparser.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* Playlist Parser */
+WaveSurfer.PlaylistParser = {
+    init: function (params) {
+        this.params = params;
+
+        var wavesurfer = this.wavesurfer = params.wavesurfer;
+
+        if (!this.wavesurfer) {
+            throw new Error('No WaveSurfer instance provided');
+        }
+
+        // parse playlist and set params
+        this.playlistFile = this.params.playlistFile || null;
+        this.playlistType = this.params.playlistType || null;
+        
+        if (this.playlistFile != null) {
+            this.parse();
+        } else{
+            throw new Error('No playlist file provided');        
+        }
+    },
+
+    parse: function() {
+        // check if playlist type is given
+        var playlist = [];
+        if (this.playlistType == 'm3u' || 'audio/mpegurl') {
+            playlist = this.playlistFile.replace(/^.*#.*$|#EXTM3U|#EXTINF:/mg, '').split('\n');
+        } else if (this.playlistType == 'pls' || 'audio/x-scpls') {
+            // to do
+        } else if (this.playlistType == 'smil' || pltype == 'application/smil'){
+            // to do
+        } else if (this.playlistType == 'json' || pltype == 'application/json'){
+		        // to do
+	      } else {
+            throw new Error('No valid playlist file provided, valid formats: m3u pls smil json');            
+        }
+
+        // playlist type is set return the playlist
+        var outputArray = [];
+        for (var i = 0; i < playlist.length; i++){
+            if (playlist[i]) {
+                // check if file name has .mp3 or .wav before adding the playlist array
+                if(playlist[i].indexOf('.mp3') !== -1 || playlist[i].indexOf('.wav') !== -1){
+                    outputArray.push(playlist[i]);
+                }
+            }
+        }
+        return outputArray;
+    }
+
+};
+
+WaveSurfer.util.extend(WaveSurfer.PlaylistParser, WaveSurfer.Observer);
