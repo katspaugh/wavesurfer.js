80f97c17268d84be1e04c6a2f32669977bb27d08
Jonathan Raoult <jesuisjonathan@gmx.fr>
2017-05-19 21:19:22 +0100

    Fixed static references to audioBufferSize
    The way the static property `audioBufferSize` is referenced (through `this`) is not valid ES2015 code and generates an error in Safari iOS at least ("IndexSizeError (DOM Exception 1): The index is not in the allowed range." since undefined is not a valid buffer size)
    My fix is using the class name but we can also use `this.constructor`

--- src/webaudio.js
+++ src/webaudio.js
@@ -219,9 +219,9 @@ export default class WebAudio extends util.Observer {
     /** @private */
     createScriptNode() {
         if (this.ac.createScriptProcessor) {
-            this.scriptNode = this.ac.createScriptProcessor(this.scriptBufferSize);
+            this.scriptNode = this.ac.createScriptProcessor(WebAudio.scriptBufferSize);
         } else {
-            this.scriptNode = this.ac.createJavaScriptNode(this.scriptBufferSize);
+            this.scriptNode = this.ac.createJavaScriptNode(WebAudio.scriptBufferSize);
         }
 
         this.scriptNode.connect(this.ac.destination);
