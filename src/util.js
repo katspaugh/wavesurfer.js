c0da3cb6cf30db5c9d2cf17d32631bd7ad79c68c
agamemnus <agamemnus@users.noreply.github.com>
2017-05-29 09:21:22 -0400

    Update util.js

--- src/util.js
+++ src/util.js
@@ -13,7 +13,7 @@ WaveSurfer.util = {
         return function () {
             var my = this, args = arguments;
             requestAnimationFrame(function () {
-                func.apply (my, args)
+                func.apply (my, args);
             });
         };
     },
