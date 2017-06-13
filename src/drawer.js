781aaeaf2d346fadab970663c1cc27f94b680ea2
agamemnus <agamemnus@users.noreply.github.com>
2017-06-13 15:02:44 -0400

    Create drawer.js

--- src/drawer.js
+++ src/drawer.js
@@ -55,7 +55,7 @@ WaveSurfer.Drawer = {
             var numerator = (clientX - bbox.left + this.wrapper.scrollLeft);
             var denominator = this.wrapper.scrollWidth - 1;
         }
-        // The clicked pixel is never equal the width. It's always 1 pixel less.
+        // The clicked pixel is never equal to the width. It's always 1 pixel less.
         // A 100-pixel element can be clicked at position 0 through position 99. And the range must include 0 as well as 1.
         // Thus, clicking at the 100th pixel (99) means progress is 1, not 99/100 or .99.
         var progress = (numerator > denominator) ? 1 : (numerator / denominator || 0);
