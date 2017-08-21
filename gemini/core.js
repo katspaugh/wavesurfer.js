59c0ccb295af0ab3345b2730e5259f39ffb3c523
Martin Spencer <mail@martinspencer.de>
2017-08-22 01:08:45 +0200

    updated tests

--- gemini/core.js
+++ gemini/core.js
@@ -1,23 +1,111 @@
-gemini.suite('core', function(core) {
+gemini.suite('core', core => {
     core
-      .setUrl('/gemini/testbed.html')
-      .setCaptureElements('.waveform');
+        .setUrl('/gemini/testbed.html')
+        .setCaptureElements('.testbed__waveform');
 
-    gemini.suite('webaudio', function(webaudio) {
+    // WebAudio test suites
+    gemini.suite('core: webaudio: basic rendering', webaudio => {
+        webaudio
+            .before(actions => actions.executeJS(function(win) {
+                win._instance = win.WaveSurfer.create({
+                    backend: 'WebAudio',
+                    container: '.testbed__waveform'
+                });
+                win._instance.load('/spec/support/demo.wav');
+            }))
+            .capture('rendering')
+            .capture('zooming', actions => actions.executeJS(function(win) {
+                win._instance.zoom(200);
+            }))
+            .capture('skipping', actions => actions.executeJS(function(win) {
+                win._instance.skip(5);
+            }));
+    });
+
+    gemini.suite('core: webaudio: custom rendering', webaudio => {
       webaudio
-        .capture('rendering', function(actions) {
-          actions.executeJS(function(window) {
-            window.cleanInstance();
-            window.createInstance({
-              backend: 'WebAudio',
-              audioFile: '/spec/support/demo.wav'
-            });
-          });
-        })
-        .capture('zooming', function(actions, find) {
-          actions.executeJS(function(window) {
-            window.INSTANCE.ws.zoom(200);
-          });
-        });
+          .before(actions => actions.executeJS(function(win) {
+              win._instance = win.WaveSurfer.create({
+                  backend: 'WebAudio',
+                  container: '.testbed__waveform',
+                  responsive: true,
+                  waveColor: 'red',
+                  progressColor: 'blue',
+                  cursorColor: 'yellow',
+                  cursorWidth: 3,
+                  minPxPerSec:  150
+              });
+              win._instance.load('/spec/support/demo.wav');
+          }))
+          .capture('rendering')
+          .capture('skipping', actions => actions.executeJS(function(win) {
+            win._instance.skip(5);
+          }));
+    });
+
+    gemini.suite('core: webaudio: responsive', webaudio => {
+      webaudio
+          .before(actions => actions.executeJS(function(win) {
+              win._instance = win.WaveSurfer.create({
+                  backend: 'WebAudio',
+                  container: '.testbed__waveform',
+                  responsive: true
+              });
+              win._instance.load('/spec/support/demo.wav');
+          }))
+          .capture('resize', actions => actions.setWindowSize(300, 1000));
+    });
+
+    // MediaElement test suites
+    gemini.suite('core: MediaElement: basic rendering', webaudio => {
+        webaudio
+            .before(actions => actions.executeJS(function(win) {
+                win._instance = win.WaveSurfer.create({
+                    backend: 'MediaElement',
+                    container: '.testbed__waveform'
+                });
+                win._instance.load('/spec/support/demo.wav');
+            }))
+            .capture('rendering')
+            .capture('zooming', actions => actions.executeJS(function(win) {
+                win._instance.zoom(200);
+            }))
+            .capture('skipping', actions => actions.executeJS(function(win) {
+                win._instance.skip(5);
+            }));
+    });
+
+    gemini.suite('core: mediaelement: custom rendering', webaudio => {
+      webaudio
+          .before(actions => actions.executeJS(function(win) {
+              win._instance = win.WaveSurfer.create({
+                  backend: 'MediaElement',
+                  container: '.testbed__waveform',
+                  responsive: true,
+                  waveColor: 'red',
+                  progressColor: 'blue',
+                  cursorColor: 'yellow',
+                  cursorWidth: 3,
+                  minPxPerSec:  150
+              });
+              win._instance.load('/spec/support/demo.wav');
+          }))
+          .capture('rendering')
+          .capture('skipping', actions => actions.executeJS(function(win) {
+            win._instance.skip(5);
+          }));
+    });
+
+    gemini.suite('core: mediaelement: responsive', webaudio => {
+      webaudio
+          .before(actions => actions.executeJS(function(win) {
+              win._instance = win.WaveSurfer.create({
+                  backend: 'MediaElement',
+                  container: '.testbed__waveform',
+                  responsive: true
+              });
+              win._instance.load('/spec/support/demo.wav');
+          }))
+          .capture('resize', actions => actions.setWindowSize(300, 1000));
     });
 });
