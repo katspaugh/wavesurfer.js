ea4e51dafd6001c8354ab1aabd8c0f95b251d0e1
Thijs Triemstra <info@collab.nl>
2018-02-20 02:18:20 +0100

    add banner to all files in dist

--- build-config/fragments/min.js
+++ build-config/fragments/min.js
@@ -3,20 +3,10 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const time = new Date();
-const pckg = require(path.join(__dirname, '..', '..', 'package.json'));
-// prettier-ignore
-const bannerPlugin = new webpack.BannerPlugin(
-`${pckg.name} ${pckg.version} (${time})
-${pckg.homepage}
-@license ${pckg.license}`
-);
-
 module.exports = {
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             sourceMap: true
-        }),
-        bannerPlugin
+        })
     ]
 };
