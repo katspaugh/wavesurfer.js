4e813e0ec0e2ad13d87a3f2e4124b94c11e71765
mronkain <mrnk@iki.fi>
2018-09-29 02:10:00 +0300

    plugins: fix microphone plugin on MS Edge  (#1463)
    * use a local temporary buffer for audio data on edge
    
    * fill buffer before clearing the wavesurfer
    
    * create the buffer only when needed
    
    * do browser check once
    
    * update changelog
    
    * fix lint error

--- src/plugin/microphone.js
+++ src/plugin/microphone.js
@@ -62,6 +62,7 @@ export default class MicrophonePlugin {
 
         this.active = false;
         this.paused = false;
+        this.browser = this.detectBrowser();
         this.reloadBufferFunction = e => this.reloadBuffer(e);
 
         // cross-browser getUserMedia
@@ -214,14 +215,15 @@ export default class MicrophonePlugin {
 
         // stop stream from device
         if (this.stream) {
-            const result = this.detectBrowser();
             // MediaStream.stop is deprecated since:
             // - Firefox 44 (https://www.fxsitecompat.com/en-US/docs/2015/mediastream-stop-has-been-deprecated/)
             // - Chrome 45 (https://developers.google.com/web/updates/2015/07/mediastream-deprecations)
             if (
-                (result.browser === 'chrome' && result.version >= 45) ||
-                (result.browser === 'firefox' && result.version >= 44) ||
-                result.browser === 'edge'
+                (this.browser.browser === 'chrome' &&
+                    this.browser.version >= 45) ||
+                (this.browser.browser === 'firefox' &&
+                    this.browser.version >= 44) ||
+                this.browser.browser === 'edge'
             ) {
                 if (this.stream.getTracks) {
                     // note that this should not be a call
@@ -239,6 +241,15 @@ export default class MicrophonePlugin {
      */
     connect() {
         if (this.stream !== undefined) {
+            // Create a local buffer for data to be copied to the Wavesurfer buffer for Edge
+            if (this.browser.browser === 'edge') {
+                this.localAudioBuffer = this.micContext.createBuffer(
+                    this.numberOfInputChannels,
+                    this.bufferSize,
+                    this.micContext.sampleRate
+                );
+            }
+
             // Create an AudioNode from the stream.
             this.mediaStreamSource = this.micContext.createMediaStreamSource(
                 this.stream
@@ -268,6 +279,10 @@ export default class MicrophonePlugin {
             this.levelChecker.disconnect();
             this.levelChecker.onaudioprocess = undefined;
         }
+
+        if (this.localAudioBuffer !== undefined) {
+            this.localAudioBuffer = undefined;
+        }
     }
 
     /**
@@ -276,7 +291,29 @@ export default class MicrophonePlugin {
     reloadBuffer(event) {
         if (!this.paused) {
             this.wavesurfer.empty();
-            this.wavesurfer.loadDecodedBuffer(event.inputBuffer);
+
+            if (this.browser.browser === 'edge') {
+                // copy audio data to a local audio buffer,
+                // from https://github.com/audiojs/audio-buffer-utils
+                let channel, l;
+                for (
+                    channel = 0,
+                        l = Math.min(
+                            this.localAudioBuffer.numberOfChannels,
+                            event.inputBuffer.numberOfChannels
+                        );
+                    channel < l;
+                    channel++
+                ) {
+                    this.localAudioBuffer
+                        .getChannelData(channel)
+                        .set(event.inputBuffer.getChannelData(channel));
+                }
+
+                this.wavesurfer.loadDecodedBuffer(this.localAudioBuffer);
+            } else {
+                this.wavesurfer.loadDecodedBuffer(event.inputBuffer);
+            }
         }
     }
 
