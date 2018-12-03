2a4421130acfad451756e6632b03f4b7176ff086
Thijs Triemstra <info@collab.nl>
2018-12-03 02:46:40 +0100

    add example update script

---
+++ scripts/update-examples.js
@@ -0,0 +1,48 @@
+/**
+ * @file update-examples.js
+ * @since 1.0.0
+ */
+
+const fs = require('fs');
+const path = require('path');
+const copydir = require('copy-dir');
+const download = require('download-tarball');
+
+const dirName = 'wavesurfer.js';
+const libDir = path.join('node_modules', dirName);
+const pjson = JSON.parse(fs.readFileSync(path.resolve(libDir, 'package.json'), 'utf8'));
+const version = pjson.version;
+const url = 'https://github.com/katspaugh/' + dirName + '/archive/' + version + '.tar.gz';
+const targetDir = '/tmp';
+const dirNameWithVersion = dirName + '-' + version;
+const targetDirUnpacked = path.join(targetDir, dirNameWithVersion);
+
+console.log('-------------------------------------------');
+console.log('Updating docs for', pjson.name, version);
+console.log('-------------------------------------------');
+console.log();
+
+// download
+let dl_options = {
+    url: url,
+    dir: targetDir
+};
+console.log('Downloading', url);
+console.log();
+download(dl_options).then(() => {
+    console.log('File downloaded and extracted at', targetDirUnpacked);
+    console.log();
+
+    // copy dist
+    copydir(path.join(targetDirUnpacked, 'example'), path.join('.', 'example'), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Updated example directory.');
+            console.log();
+        }
+    });
+}).catch(err => {
+    console.log('File could not be downloaded properly!');
+    console.log(err);
+});
