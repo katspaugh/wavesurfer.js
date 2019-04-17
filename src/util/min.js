efdd0d29cd37916ee313dbac8e943dd37493f5ac
Thijs Triemstra <info@collab.nl>
2019-04-17 22:09:13 +0200

    doc update

--- src/util/min.js
+++ src/util/min.js
@@ -2,7 +2,7 @@
  * Get the smallest value
  *
  * @param   {Array} values Array of numbers
- * @returns {Number}       Smallest number found
+ * @returns {Number} Smallest number found
  * @example console.log(min([1, 2, 3])); // logs 1
  */
 export default function min(values) {
