b5c58c43e674d8b2d8ce958bec7a0af43d8c90c6
Thijs Triemstra <info@collab.nl>
2019-05-26 15:07:15 +0200

    fix lint errors

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -1,7 +1,11 @@
+/**
+ * @since 3.0.0
+ */
+
 import Observer from './observer';
 
 /**
- * Perform an fetch request
+ * Perform a request using fetch.
  *
  * @param {Options} options Description
  * dofetch({
@@ -11,8 +15,8 @@ import Observer from './observer';
  * @returns {Object} Observer instance
  */
 export default function dofetch(options) {
+    const instance = new Observer();
     if (self.fetch) {
-        const instance = new Observer();
         const fetchRequest = new Request(options.url);
         let fetchHeaders = new Headers();
 
@@ -28,32 +32,35 @@ export default function dofetch(options) {
 
         // set default fetch options
         const fetchOptions = {
-            method: (options.method || "GET"),
+            method: options.method || 'GET',
             headers: fetchHeaders,
-            mode: (options.mode || "cors"),
-            credentials: (options.credentials || "same-origin"),
-            cache: (options.cache || "default")
+            mode: options.mode || 'cors',
+            credentials: options.credentials || 'same-origin',
+            cache: options.cache || 'default'
         };
 
         // do the fetch
-        fetch(fetchRequest,fetchOptions).then(function(response) {
-          if (response.ok) {
-            return response.blob();
-          } else{
-            instance.fireEvent('HTTP error status ', response.status)
-          }
-        }).then(function(blobResponse) {        
-            var objectURL = URL.createObjectURL(blobResponse);
-            instance.fireEvent('load', objectURL);
-            instance.fireEvent('success', fetchRequest.response, objectURL);
-        }).catch(function(error) {
-            instance.fireEvent('error', error.message)
-        });
+        fetch(fetchRequest, fetchOptions)
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                } else {
+                    instance.fireEvent('HTTP error status ', response.status);
+                }
+            })
+            .then(blobResponse => {
+                var objectURL = URL.createObjectURL(blobResponse);
+                instance.fireEvent('load', objectURL);
+                instance.fireEvent('success', fetchRequest.response, objectURL);
+            })
+            .catch(error => {
+                instance.fireEvent('error', error.message);
+            });
 
         // return the fetch
         instance.fetchRequest = fetchRequest;
-        return instance;
-    } else{
+    } else {
         instance.fireEvent('error', 'fetch api not supported');
     }
+    return instance;
 }
