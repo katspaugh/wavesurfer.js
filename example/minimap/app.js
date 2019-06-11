a29246711d122801dcf6f73c57d99773873c4997
Thijs Triemstra <info@collab.nl>
2019-06-11 17:23:54 +0200

    simplify

--- example/minimap/app.js
+++ example/minimap/app.js
@@ -20,37 +20,12 @@ function init() {
         ]
     };
 
-    if (location.search.match('scroll')) {
-        options.minPxPerSec = 100;
-        options.scrollParent = true;
-    }
-
-    if (location.search.match('normalize')) {
-        options.normalize = true;
-    }
-
     // create an instance
     wavesurfer = WaveSurfer.create(options);
 
-    /* Progress bar */
-    (function() {
-        var progressDiv = document.querySelector('#progress-bar');
-        var progressBar = progressDiv.querySelector('.progress-bar');
-
-        var showProgress = function(percent) {
-            progressDiv.style.display = 'block';
-            progressBar.style.width = percent + '%';
-        };
-
-        var hideProgress = function() {
-            progressDiv.style.display = 'none';
-        };
-
-        wavesurfer.on('loading', showProgress);
-        wavesurfer.on('ready', hideProgress);
-        wavesurfer.on('destroy', hideProgress);
-        wavesurfer.on('error', hideProgress);
-    })();
+    document
+        .querySelector('[data-action="play"]')
+        .addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
 
     wavesurfer.load('../media/demo.wav');
 }
