b166706602e2ad9aa8be4b61b89a32f05ef626d0
Thijs Triemstra <info@collab.nl>
2019-07-10 22:18:06 +0200

    examples: add example using barWidth option

---
+++ example/bars/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Create an instance
+var wavesurfer = {};
+
+// Init & load audio file
+document.addEventListener('DOMContentLoaded', function() {
+    wavesurfer = WaveSurfer.create({
+        container: document.querySelector('#waveform'),
+        barWidth: 2,
+        barHeight: 1,
+        barGap: null
+    });
+
+    wavesurfer.on('error', function(e) {
+        console.warn(e);
+    });
+
+    // Load audio from URL
+    wavesurfer.load('../media/demo.wav');
+
+    // Play button
+    var button = document.querySelector('[data-action="play"]');
+
+    button.addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
+});
