ab73d0900d5b9b23c7cca426f73d742288c32698
Thijs Triemstra <info@collab.nl>
2019-10-15 15:11:46 +0200

    throw error when invalid url is passed to MediaElement backend (#1775)
    * throw error when invalid url is passed to MediaElement backend
    
    * update changelog
    
    * address review comments
    
    * fix lint error

--- spec/mediaelement.spec.js
+++ spec/mediaelement.spec.js
@@ -278,3 +278,31 @@ describe('WaveSurfer/MediaElement:', function() {
         loadElement();
     });
 });
+
+/** @test {WaveSurfer} */
+describe('WaveSurfer/MediaElement/errors:', function() {
+    var element;
+
+    beforeEach(function() {
+        element = TestHelpers.createElement('test');
+    });
+
+    afterEach(function() {
+        TestHelpers.removeElement(element);
+    });
+
+    /**
+     * @test {WaveSurfer}
+     */
+    it('throw when load is called with invalid HTMLMediaElement', function() {
+        var wave = TestHelpers.createWaveform({
+            container: '#test',
+            backend: 'MediaElement'
+        });
+        expect(function() {
+            wave[0].load({
+                foo: 'bar'
+            });
+        }).toThrow(new Error('media parameter is not a valid media element'));
+    });
+});
