7a9d4fa675caa877ddcb9e88083bd2e4d5041e0b
Mustafa J <mustafa.0x@gmail.com>
2019-12-11 11:07:13 +0200

    readd util.extend; warn when used

---
+++ src/util/extend.js
@@ -0,0 +1,12 @@
+/**
+ * Extend an object shallowly with others
+ *
+ * @param {Object} dest The target object
+ * @param {Object[]} sources The objects to use for extending
+ *
+ * @return {Object} Merged object
+ */
+export default function extend(dest, ...sources) {
+    console.warn('util.extend is deprecated; use Object.assign');
+    return Object.assign(dest, ...sources);
+}
