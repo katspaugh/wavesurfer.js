c333315c15f7157e2f28e76bd338ba553d95147e
marizuccara <mari.zuccara@gmail.com>
2020-07-06 08:52:09 +0200

    removed util.extend functionality as deprecated since 3.3.0 (can be replaced by Object.assign instead)

--- src/util/index.js
+++ src/util/index.js
@@ -3,7 +3,6 @@ export { default as getId } from './get-id';
 export { default as max } from './max';
 export { default as min } from './min';
 export { default as Observer } from './observer';
-export { default as extend } from './extend';
 export { default as style } from './style';
 export { default as requestAnimationFrame } from './request-animation-frame';
 export { default as frame } from './frame';
