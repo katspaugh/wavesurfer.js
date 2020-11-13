fd79790980a40704ca4f0e4d44861671df8266bb
Claudio Santos <claudiohbsantos@gmail.com>
2020-11-12 19:55:05 -0500

    Extract absolute maximum as a utility function

---
+++ src/util/absMax.js
@@ -0,0 +1,15 @@
+import utilMax from './max';
+import utilmin from './min';
+
+/**
+ * Get the largest absolute value
+ *
+ * @param   {Array} values Array of numbers
+ * @returns {Number} Largest number found
+ * @example console.log(max([-3, 2, 1]), max([-3, 2, 4])); // logs 3 4
+ */
+export default function absMax(values) {
+    const max = utilMax(values);
+    const min = utilmin(values);
+    return -min > max ? -min : max;
+}
