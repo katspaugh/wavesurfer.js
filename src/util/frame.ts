543707f52d13989d3e4e8eed6f87eb9a03872832
sundayz <sundays@wagn.me>
2020-11-23 21:39:49 +0000

    convert frame.js to typescript

--- src/util/frame.ts
+++ src/util/frame.ts
@@ -4,10 +4,10 @@ import reqAnimationFrame from './request-animation-frame';
  * Create a function which will be called at the next requestAnimationFrame
  * cycle
  *
- * @param {function} func The function to call
+ * @param func The function to call
  *
- * @return {func} The function wrapped within a requestAnimationFrame
+ * @return The function wrapped within a requestAnimationFrame
  */
-export default function frame(func) {
-    return (...args) => reqAnimationFrame(() => func(...args));
+export default function frame(func: Function): Function {
+    return (...args: any[]) => reqAnimationFrame(() => func(...args));
 }
