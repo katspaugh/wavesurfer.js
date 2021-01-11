faf656ce7ad288bb8e0b059d865b09f1911522b4
Adarsh Pastakia <adarshpastakia@outlook.com>
2021-01-11 14:56:11 +0400

    Added split channel examples for bars

--- example/bars/main.js
+++ example/bars/main.js
@@ -2,6 +2,7 @@
 
 // Create an instance
 let wavesurfer = {};
+let wavesurferWithOptions;
 
 // Init & load audio file
 document.addEventListener('DOMContentLoaded', function() {
@@ -23,4 +24,41 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('[data-action="play"]');
 
     button.addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
+
+
+    // WaveSurfer with options example
+    wavesurferWithOptions = WaveSurfer.create({
+        container: document.querySelector('#waveform-with-options'),
+        barWidth: 2,
+        barHeight: 1,
+        barGap: null,
+        splitChannels: true,
+        splitChannelsOptions: {
+            overlay: false,
+            channelColors: {
+                0: {
+                    progressColor: 'green',
+                    waveColor: 'pink'
+                },
+                1: {
+                    progressColor: 'orange',
+                    waveColor: 'purple'
+                }
+            },
+            filterChannels: [],
+            relativeNormalization: true
+        }
+    });
+
+    wavesurferWithOptions.on('error', function(e) {
+        console.warn(e);
+    });
+
+    // Load audio from URL
+    wavesurferWithOptions.load('../media/stereo.mp3');
+
+    // Play/pause on button press
+    document
+        .getElementById('play-button')
+        .addEventListener('click', wavesurferWithOptions.playPause.bind(wavesurferWithOptions));
 });
