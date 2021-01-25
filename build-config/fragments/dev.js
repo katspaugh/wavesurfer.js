3d645f6655dbd2c896b27933e8db52d8cb689d47
John Paul Medina <jp@leadtrust.io>
2021-01-25 16:43:41 -0500

    Remove unnecessary options on webpack dev config

--- build-config/fragments/dev.js
+++ build-config/fragments/dev.js
@@ -1,18 +1,14 @@
 /* eslint-env node */
 
 const path = require('path');
-const rootDir = path.resolve(__dirname, '..', '..');
 
 module.exports = {
     mode: 'development',
     devtool: 'eval-source-map',
-    output: {
-        publicPath: 'localhost:8080/'
-    },
     devServer: {
         static: [
             {
-                directory: rootDir,
+                directory: path.resolve(__dirname, '..', '..'),
                 staticOptions: {},
                 serveIndex: true,
                 watch: {
