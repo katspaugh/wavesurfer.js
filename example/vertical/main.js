689425c509daba4e54b302b626bdbf7be060d687
valrus <imccowan@gmail.com>
2021-02-23 22:43:06 -0800

    Remove test regions from vertical example

--- example/vertical/main.js
+++ example/vertical/main.js
@@ -10,30 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
         container: document.querySelector('#waveform'),
         waveColor: '#A8DBA8',
         progressColor: '#3B8686',
-        vertical: true,
-        plugins: [
-            WaveSurfer.regions.create({
-                regionsMinLength: 2,
-                regions: [
-                    {
-                        start: 1,
-                        end: 3,
-                        loop: false,
-                        color: 'hsla(400, 100%, 30%, 0.5)'
-                    },
-                    {
-                        start: 5,
-                        end: 7,
-                        loop: false,
-                        color: 'hsla(200, 50%, 70%, 0.4)',
-                        minLength: 1
-                    }
-                ],
-                dragSelection: {
-                    slop: 5
-                }
-            })
-        ]
+        vertical: true
     });
 
     wavesurfer.on('error', function(e) {
