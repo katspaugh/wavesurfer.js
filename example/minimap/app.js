861d52ad674e41f34b35912da82abef57a6f230b
Ben Osheroff <osheroff@users.noreply.github.com>
2021-03-19 08:16:01 -0700

    examples: use var instead of let (#2227)
    * get examples working again
    
    many of them are sharing a wavesurfer instance with `trivia.js` via a
    global variable.  I can't say this is the best idea, but this is at
    least explicit about it.
    
    * CR: change to no-var disable in examples

--- example/minimap/app.js
+++ example/minimap/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let wavesurfer;
+var wavesurfer;
 
 function init() {
     // configure
--- example/trivia.js
+++ example/trivia.js
@@ -1,20 +1,20 @@
-var wavesurfer = window.wavesurfer; // eslint-disable-line no-var
+let ws = window.wavesurfer;
 
-let GLOBAL_ACTIONS = {
+var GLOBAL_ACTIONS = { // eslint-disable-line
     play: function() {
-        wavesurfer.playPause();
+        window.wavesurfer.playPause();
     },
 
     back: function() {
-        wavesurfer.skipBackward();
+        window.wavesurfer.skipBackward();
     },
 
     forth: function() {
-        wavesurfer.skipForward();
+        window.wavesurfer.skipForward();
     },
 
     'toggle-mute': function() {
-        wavesurfer.toggleMute();
+        window.wavesurfer.toggleMute();
     }
 };
 
@@ -58,6 +58,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Navbar links
     let ul = document.querySelector('.nav-pills');
+    if ( !ul ) {
+        return;
+    }
+
     let pills = ul.querySelectorAll('li');
     let active = pills[0];
     if (location.search) {
