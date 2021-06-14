733470d279812aee536c10fedd40d1c724ab9e4d
Antonio Felguerez <antoniof@spotify.com>
2021-06-14 08:58:58 -0500

    Fix variable scope error

--- src/plugin/spectrogram/index.js
+++ src/plugin/spectrogram/index.js
@@ -256,30 +256,30 @@ export default class SpectrogramPlugin {
         const heightFactor = my.buffer ? 2 / my.buffer.numberOfChannels : 1;
         if (spectrCc) {
             const imageData = spectrCc.createImageData(width, height);
-        }
-        let i;
-        let j;
-        let k;
+            let i;
+            let j;
+            let k;
 
-        for (i = 0; i < pixels.length; i++) {
-            for (j = 0; j < pixels[i].length; j++) {
-                const colorMap = my.colorMap[pixels[i][j]];
-                /* eslint-disable max-depth */
-                for (k = 0; k < heightFactor; k++) {
-                    let y = height - j * heightFactor;
-                    if (heightFactor === 2 && k === 1) {
-                        y--;
+            for (i = 0; i < pixels.length; i++) {
+                for (j = 0; j < pixels[i].length; j++) {
+                    const colorMap = my.colorMap[pixels[i][j]];
+                    /* eslint-disable max-depth */
+                    for (k = 0; k < heightFactor; k++) {
+                        let y = height - j * heightFactor;
+                        if (heightFactor === 2 && k === 1) {
+                            y--;
+                        }
+                        const redIndex = y * (width * 4) + i * 4;
+                        imageData.data[redIndex] = colorMap[0] * 255;
+                        imageData.data[redIndex + 1] = colorMap[1] * 255;
+                        imageData.data[redIndex + 2] = colorMap[2] * 255;
+                        imageData.data[redIndex + 3] = colorMap[3] * 255;
                     }
-                    const redIndex = y * (width * 4) + i * 4;
-                    imageData.data[redIndex] = colorMap[0] * 255;
-                    imageData.data[redIndex + 1] = colorMap[1] * 255;
-                    imageData.data[redIndex + 2] = colorMap[2] * 255;
-                    imageData.data[redIndex + 3] = colorMap[3] * 255;
+                    /* eslint-enable max-depth */
                 }
-                /* eslint-enable max-depth */
             }
+            spectrCc.putImageData(imageData, 0, 0);
         }
-        spectrCc.putImageData(imageData, 0, 0);
     }
 
     getFrequencies(callback) {
