e8163bb9ab2464b03340c91a1dbdd88704badec4
Thijs Triemstra <info@collab.nl>
2021-08-02 17:00:31 +0200

    globalObject causes build issue

--- build-config/fragments/common.js
+++ build-config/fragments/common.js
@@ -20,8 +20,7 @@ module.exports = {
     mode: 'development',
     output: {
         libraryTarget: 'umd',
-        umdNamedDefine: true,
-        globalObject: 'this'
+        umdNamedDefine: true
     },
     performance: {
         hints: false
