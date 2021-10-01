8bd66aeff0f851d61ac7d1ded0b6b87b5c5acd96
Enton Biba <entonbiba@users.noreply.github.com>
2021-09-30 21:08:52 -0400

    Update accessibility.js

--- src/util/accessibility.js
+++ src/util/accessibility.js
@@ -4,48 +4,46 @@
  * @since 5.2.0
  */
 
-/**
- * Create the announcement element
- */
-export default function createAnnouncementElement() {
-    var announcement = document.getElementById("announce-waveform");
-    var id = "announce-waveform";
-    
-    if(!announcement){
-        announcement = document.createElement("div");        
-        announcement.setAttribute("id", id);
-        announcement.setAttribute("aria-live", "polite");
-        var styles = {
-            position: 'absolute',
-            width: '1px',
-            height: '1px',
-            padding: '0',
-            margin: '-1',
-            overflow: 'hidden',
-            clip: 'rect(0,0,0,0)',
-            white-space: 'nowrap',
-            border: '0'            
-        };
-        Object.keys(styles).forEach(prop => {
-            if (announcement.style[prop] !== styles[prop]) {
-                announcement.style[prop] = styles[prop];
-            }
-        });
-        document.body.appendChild(announcement);
-    }
-    document.getElementById(id).innerHTML = "Loading waveform";
+import style from './style';
+
+export class Accessibility {
+  constructor() {
+    this.announcement = document.body.appendChild(
+      document.createElement('div')
+    );
+    this.announcement.setAttribute('aria-live', 'polite');
+
+    style(this.announcement, {
+      position: 'absolute',
+      width: '1px',
+      height: '1px',
+      padding: '0',
+      margin: '-1',
+      overflow: 'hidden',
+      clip: 'rect(0,0,0,0)',
+      white-space: 'nowrap',
+      border: '0'            
+    });
+
+    this.updateAnnouncementText('Loading waveform');
+  }
+
+  // update the announcement text element
+  updateAnnouncementText(text) {
+    this.announcement.textContent = text;
+  }
+
+  // clear the announcement text element
+  clearAnnouncementText() {
+    this.announcement.textContent = '';
+  }
+
+  // remove the announcement text element
+  removeAnnouncementText() {
+    this.announcement.remove();
+  }
 }
 
-/**
- * Update the announcement text
- */
-export default function updateAnnouncementText(message) {
-    document.getElementById("announce-waveform").innerHTML = message;
-}
 
-/**
- * Clear the announcement text
- */
-export default function clearAnnouncementText() {
-    document.getElementById("announce-waveform").innerHTML = "";
-}
+
+
