3bef466bf851c7681cbf4a6c1e96f0b9ad2a3925
Enton Biba <entonbiba@users.noreply.github.com>
2021-09-30 21:41:25 -0400

    Update accessibility.js

--- src/util/accessibility.js
+++ src/util/accessibility.js
@@ -7,41 +7,41 @@
 import style from './style';
 
 export class Accessibility {
-  constructor() {
-    this.announcement = document.body.appendChild(
-      document.createElement('div')
-    );
-    this.announcement.setAttribute('aria-live', 'polite');
+    constructor() {
+        this.announcement = document.body.appendChild(
+            document.createElement('div')
+        );
+        this.announcement.setAttribute('aria-live', 'polite');
 
-    style(this.announcement, {
-      position: "absolute",
-      width: "1px",
-      height: "1px",
-      padding: "0",
-      margin: "-1",
-      overflow: "hidden",
-      clip: "rect(0,0,0,0)",
-      "white-space": "nowrap",
-      border: "0"
-    });
+        style(this.announcement, {
+            position: "absolute",
+            width: "1px",
+            height: "1px",
+            padding: "0",
+            margin: "-1",
+            overflow: "hidden",
+            clip: "rect(0,0,0,0)",
+            "white-space": "nowrap",
+            border: "0"
+        });
 
-    this.updateAnnouncementText('Loading waveform');
-  }
+        this.updateAnnouncementText('Loading waveform');
+    }
 
-  // update the announcement text element
-  updateAnnouncementText(text) {
-    this.announcement.textContent = text;
-  }
+    // update the announcement text element
+    updateAnnouncementText(text) {
+        this.announcement.textContent = text;
+    }
 
-  // clear the announcement text element
-  clearAnnouncementText() {
-    this.announcement.textContent = '';
-  }
+    // clear the announcement text element
+    clearAnnouncementText() {
+        this.announcement.textContent = '';
+    }
 
-  // remove the announcement text element
-  destroy() {
-    this.announcement.remove();
-  }
+    // remove the announcement text element
+        destroy() {
+        this.announcement.remove();
+    }
 }
 
 
