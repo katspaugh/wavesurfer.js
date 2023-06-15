3d30b0b982b50e0584fe186c06e1400b9cd368fa
Pavlo B <zetorama@gmail.com>
2023-06-15 04:59:31 -0700

    Allow providing mimeType to RecordPlugin (#2892)

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -2,8 +2,13 @@ async function fetchArrayBuffer(url: string): Promise<ArrayBuffer> {
   return fetch(url).then((response) => response.arrayBuffer())
 }
 
+async function fetchBlob(url: string): Promise<Blob> {
+  return fetch(url).then((response) => response.blob())
+}
+
 const Fetcher = {
   fetchArrayBuffer,
+  fetchBlob,
 }
 
 export default Fetcher
