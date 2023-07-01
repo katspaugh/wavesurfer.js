b682e00481806ef965766b78c349faf1de931f6a
katspaugh <katspaugh@gmail.com>
2023-07-01 21:37:10 +0200

    Chore: upgrade dev dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 import { addMatchImageSnapshotPlugin } from 'cypress-image-snapshot/plugin'
 
 export default defineConfig({
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
