a012cdc1329d1d8c52df1025e2b5c222a7c646a7
katspaugh <katspaugh@gmail.com>
2023-07-29 11:58:47 +0200

    7.1.0

--- scripts/plugin.sh
+++ scripts/plugin.sh
@@ -12,4 +12,4 @@ fi
 
 FILE_NAME=$(echo "$PLUGIN_NAME" | sed -e 's/\(.*\)/\L\1/')
 
-cat ./scripts/plugin-template.ts.template | sed "s/PluginName/$PLUGIN_NAME/g" > "./src/plugins/${FILE_NAME}.ts"
+cat ./scripts/plugin-template.ts.template | sed "s/Template/$PLUGIN_NAME/g" > "./src/plugins/${FILE_NAME}.ts"
