a346f9d585607563bfd1d0c1d4afc81a21918c4e
Robin Fernandes <robin@soal.org>
2023-08-20 22:30:27 +1000

    Add test for case where spectrogram container is specified explicitly

--- cypress/e2e/spectrogram.cy.js
+++ cypress/e2e/spectrogram.cy.js
@@ -52,4 +52,42 @@ describe('WaveSurfer Spectrogram plugin tests', () => {
       })
     })
   })
+
+  it('should render a spectrogram when initialised with an explicit container', () => {
+    cy.visit('cypress/e2e/index.html')
+    cy.window().then((win) => {
+      return new Promise((resolve) => {
+
+        // Add a container for the spectrogram
+        const title = win.document.createElement('h2')
+        title.innerText = 'Spectrogram'
+        win.document.querySelector("body").appendChild(title)
+        const spectrogram = win.document.createElement('div')
+        spectrogram.id = 'my_spectrogram'
+        spectrogram.innerText = 'Spectrogram will go here'
+        win.document.querySelector("body").appendChild(spectrogram)
+
+
+        win.wavesurfer = win.WaveSurfer.create({
+          container: id,
+          height: 200,
+          plugins: [win.Spectrogram.create({
+            container: '#my_spectrogram',
+            height: 200,
+            labels: true,
+          })],
+        })
+
+        // Load a file and unhide the div
+        win.wavesurfer.load('../../examples/audio/demo.wav')
+        win.document.querySelector(id).style.display = 'inline-block'
+
+        // Ensure we display the spectrogram successfully
+        win.wavesurfer.once('ready', () => {
+          cy.get("body").matchImageSnapshot('spectrogram-explicit-container')
+          resolve()
+        })
+      })
+    })
+  })  
 })
