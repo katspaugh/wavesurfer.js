4942324061a8a7d1799b408fa3792b10ec9a1f8e
katspaugh <katspaugh@gmail.com>
2023-12-28 12:08:57 +0100

    Add comments

--- src/base-plugin.ts
+++ src/base-plugin.ts
@@ -7,17 +7,22 @@ export type BasePluginEvents = {
 
 export type GenericPlugin = BasePlugin<BasePluginEvents, unknown>
 
+/** Base class for wavesurfer plugins */
 export class BasePlugin<EventTypes extends BasePluginEvents, Options> extends EventEmitter<EventTypes> {
   protected wavesurfer?: WaveSurfer
   protected subscriptions: (() => void)[] = []
   protected options: Options
 
+  /** Create a plugin instance */
   constructor(options: Options) {
     super()
     this.options = options
   }
 
-  protected onInit() {}
+  /** Called after this.wavesurfer is available */
+  protected onInit() {
+    return
+  }
 
   /** Do not call directly, only called by WavesSurfer internally */
   public _init(wavesurfer: WaveSurfer) {
@@ -25,6 +30,7 @@ export class BasePlugin<EventTypes extends BasePluginEvents, Options> extends Ev
     this.onInit()
   }
 
+  /** Destroy the plugin and unsubscribe from all events */
   public destroy() {
     this.emit('destroy')
     this.subscriptions.forEach((unsubscribe) => unsubscribe())
