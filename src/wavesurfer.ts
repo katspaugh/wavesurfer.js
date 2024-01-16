097e0f3f06762b85a67d7aaa5f08f34033b2d08d
Brian Byassee <brianbyassee@brians-mbp-2.lan>
2024-01-16 13:25:30 -0700

    Feat: exports for Base Plugin

--- src/wavesurfer.ts
+++ src/wavesurfer.ts
@@ -517,4 +517,4 @@ class WaveSurfer extends Player<WaveSurferEvents> {
 export default WaveSurfer
 
 export { default as BasePlugin } from './base-plugin.js'
-export { default as render } from './dom.js'
+export { default as createElement } from './dom.js'
