86bafb19cb29d0db10f166b4f7b0ae74eb1a36f9
Alexander-Lee Couchy <43746763+alexanderleecii@users.noreply.github.com>
2024-06-16 15:24:31 +0200

    Update scripts/clean.cjs
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>

--- scripts/clean.cjs
+++ scripts/clean.cjs
@@ -1,9 +1,9 @@
-const path = require("path")
-const fs = require("fs")
+const path = require('path')
+const fs = require('fs')
 
 const run = () => {
-    const distPath = path.join(__dirname, '../dist')
-    fs.rmSync(distPath, { recursive: true, force: true })
+  const distPath = path.join(__dirname, '../dist')
+  fs.rmSync(distPath, { recursive: true, force: true })
 }
 
 run()
