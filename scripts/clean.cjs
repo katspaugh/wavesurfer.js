08afa79b8ead606ed732afb2acb020ab89ddf4a3
Alexander-Lee Couchy <43746763+alexanderleecii@users.noreply.github.com>
2024-06-17 10:30:43 +0200

    Make yarn build command cross-platform (#3747)
    * Make npm build command cross-platform
    
    * Update scripts/clean.cjs
    
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: katspaugh <381895+katspaugh@users.noreply.github.com>

---
+++ scripts/clean.cjs
@@ -0,0 +1,9 @@
+const path = require('path')
+const fs = require('fs')
+
+const run = () => {
+  const distPath = path.join(__dirname, '../dist')
+  fs.rmSync(distPath, { recursive: true, force: true })
+}
+
+run()
