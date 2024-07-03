c8f62917bf99b21aaf4d24d20a07d65aeb8c02af
katspaugh <katspaugh@gmail.com>
2024-07-03 10:57:41 +0300

    Fix: [Regions] export Region class for TypeScript docs

--- src/plugins/regions.ts
+++ src/plugins/regions.ts
@@ -65,7 +65,7 @@ export type RegionParams = {
   contentEditable?: boolean
 }
 
-class SingleRegion extends EventEmitter<RegionEvents> {
+class Region extends EventEmitter<RegionEvents> {
   public element: HTMLElement
   public id: string
   public start: number
@@ -80,7 +80,11 @@ class SingleRegion extends EventEmitter<RegionEvents> {
   public contentEditable = false
   public subscriptions: (() => void)[] = []
 
-  constructor(params: RegionParams, private totalDuration: number, private numberOfChannels = 0) {
+  constructor(
+    params: RegionParams,
+    private totalDuration: number,
+    private numberOfChannels = 0,
+  ) {
     super()
 
     this.subscriptions = []
@@ -586,7 +590,7 @@ class RegionsPlugin extends BasePlugin<RegionsPluginEvents, RegionsPluginOptions
 
     const duration = this.wavesurfer.getDuration()
     const numberOfChannels = this.wavesurfer?.getDecodedData()?.numberOfChannels
-    const region = new SingleRegion(options, duration, numberOfChannels)
+    const region = new Region(options, duration, numberOfChannels)
 
     if (!duration) {
       this.subscriptions.push(
@@ -639,7 +643,7 @@ class RegionsPlugin extends BasePlugin<RegionsPluginEvents, RegionsPluginOptions
         const end = ((x + initialSize) / width) * duration
 
         // Create a region but don't save it until the drag ends
-        region = new SingleRegion(
+        region = new Region(
           {
             ...options,
             start,
@@ -678,5 +682,3 @@ class RegionsPlugin extends BasePlugin<RegionsPluginEvents, RegionsPluginOptions
 }
 
 export default RegionsPlugin
-
-export type Region = InstanceType<typeof SingleRegion>
