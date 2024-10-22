41ba375ffc5fdca044032fb20023d24295370f82
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2024-10-22 22:18:56 +0200

    fix(timeline): Render timeline ticks based on scroll position
    Uses current scroll position instead of 0 when initially rendering timeline. Previously, the initial render
    would show ticks starting from 0 regardless of scroll position.
    
    Closes #3782

--- src/plugins/timeline.ts
+++ src/plugins/timeline.ts
@@ -163,7 +163,9 @@ class TimelinePlugin extends BasePlugin<TimelinePluginEvents, TimelinePluginOpti
 
     setTimeout(() => {
       if (!this.wavesurfer) return
-      renderIfVisible(0, this.wavesurfer?.getWidth() || 0)
+      const scrollLeft = this.wavesurfer.getScroll()
+      const scrollRight = scrollLeft + this.wavesurfer.getWidth()
+      renderIfVisible(scrollLeft, scrollRight)
       this.subscriptions.push(
         this.wavesurfer.on('scroll', (_start, _end, scrollLeft, scrollRight) => {
           renderIfVisible(scrollLeft, scrollRight)
