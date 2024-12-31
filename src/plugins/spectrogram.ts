0ffead9ed1cefacc732de46256bde69694aa30cf
katspaugh <381895+katspaugh@users.noreply.github.com>
2024-12-31 09:46:07 +0300

    Docs(Spectrogram): add frequenciesDataUrl option docs

--- src/plugins/spectrogram.ts
+++ src/plugins/spectrogram.ts
@@ -291,6 +291,8 @@ export type SpectrogramPluginOptions = {
   | 'roseus'
   /** Render a spectrogram for each channel independently when true. */
   splitChannels?: boolean
+  /** URL with pre-computed spectrogram JSON data, the data must be a Uint8Array[][] **/
+  frequenciesDataUrl?: string
 }
 
 export type SpectrogramPluginEvents = BasePluginEvents & {
