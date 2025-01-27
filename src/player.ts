1aa014dd9e9d719c86c4ac754b708c6daf4b498f
Stian Larsen <71408982+Stianlars1@users.noreply.github.com>
2025-01-27 15:48:47 +0100

    Fix: don't pause audio for external media on destroy() (#4012)
    * Updated player.ts to not pause audio for external media on destroy()
    
    In today's solution the playback audio will pause on calling destroy() for instances using external media. 
    
    We probably dont need this for the external media's.
    
    * Removed whitespace

--- src/player.ts
+++ src/player.ts
@@ -86,9 +86,8 @@ class Player<T extends GeneralEventTypes> extends EventEmitter<T> {
   }
 
   protected destroy() {
-    this.media.pause()
-
     if (this.isExternalMedia) return
+    this.media.pause()
     this.media.remove()
     this.revokeSrc()
     this.media.src = ''
