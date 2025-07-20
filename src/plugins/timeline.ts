c9d723974f15de86b85536f78a5b9f541b1f7afa
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-07-20 08:41:15 +0200

    fix(timeline): remove notch listeners on redraw (#4163)

--- src/plugins/timeline.ts
+++ src/plugins/timeline.ts
@@ -56,6 +56,7 @@ export type TimelinePluginEvents = BasePluginEvents & {
 
 class TimelinePlugin extends BasePlugin<TimelinePluginEvents, TimelinePluginOptions> {
   private timelineWrapper: HTMLElement
+  private unsubscribeNotches: (() => void)[] = []
   protected options: TimelinePluginOptions & typeof defaultOptions
 
   constructor(options?: TimelinePluginOptions) {
@@ -102,6 +103,8 @@ class TimelinePlugin extends BasePlugin<TimelinePluginEvents, TimelinePluginOpti
 
   /** Unmount */
   public destroy() {
+    this.unsubscribeNotches.forEach((unsubscribe) => unsubscribe())
+    this.unsubscribeNotches = []
     this.timelineWrapper.remove()
     super.destroy()
   }
@@ -170,7 +173,7 @@ class TimelinePlugin extends BasePlugin<TimelinePluginEvents, TimelinePluginOpti
 
     renderIfVisible(scrollLeft, scrollRight)
 
-    this.subscriptions.push(
+    this.unsubscribeNotches.push(
       this.wavesurfer.on('scroll', (_start, _end, scrollLeft, scrollRight) => {
         renderIfVisible(scrollLeft, scrollRight)
       }),
@@ -178,6 +181,9 @@ class TimelinePlugin extends BasePlugin<TimelinePluginEvents, TimelinePluginOpti
   }
 
   private initTimeline() {
+    this.unsubscribeNotches.forEach((unsubscribe) => unsubscribe())
+    this.unsubscribeNotches = []
+
     const duration = this.wavesurfer?.getDuration() ?? this.options.duration ?? 0
     const pxPerSec = (this.wavesurfer?.getWrapper().scrollWidth || this.timelineWrapper.scrollWidth) / duration
     const timeInterval = this.options.timeInterval ?? this.defaultTimeInterval(pxPerSec)
